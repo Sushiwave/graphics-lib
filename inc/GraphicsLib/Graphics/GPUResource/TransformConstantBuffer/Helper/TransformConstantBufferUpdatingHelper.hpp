c38ee74bddde6dc09e685a791548238d6ab1b0c9
Ryota Nakashima <b1615946@planet.kanazawa-it.ac.jp>
2020-11-17 13:25:44 +0900

    change: 新規store関数を追加

--- inc/GraphicsLib/Graphics/GPUResource/TransformConstantBuffer/Helper/TransformConstantBufferUpdatingHelper.hpp
+++ inc/GraphicsLib/Graphics/GPUResource/TransformConstantBuffer/Helper/TransformConstantBufferUpdatingHelper.hpp
@@ -11,8 +11,20 @@ namespace cg
 	{
 	public:
 		static void storeWVP(DirectX::XMFLOAT4X4* wvp, const Transform& transform, const Camera& camera);
+		static void storeInvWVP(DirectX::XMFLOAT4X4* invWVP, const Transform& transform, const Camera& camera);
+
 		static void storeW(DirectX::XMFLOAT4X4* w, const Transform& transform);
+		static void storeInvW(DirectX::XMFLOAT4X4* invW, const Transform& transform);
+
+		static void storeV(DirectX::XMFLOAT4X4* v, const Camera& camera);
+		static void storeInvV(DirectX::XMFLOAT4X4* invV, const Camera& camera);
+
+		static void storeP(DirectX::XMFLOAT4X4* p, const Camera& camera);
+		static void storeInvP(DirectX::XMFLOAT4X4* invP, const Camera& camera);
+
 		static void storeVP(DirectX::XMFLOAT4X4* vp, const Camera& camera);
+		static void storeInvVP(DirectX::XMFLOAT4X4* invVP, const Camera& camera);
+
 		static void storeN(DirectX::XMFLOAT4X4* n, const Transform& transform);
 	};
 }
--- src/GraphicsLib/Graphics/GPUResource/TransformConstantBuffer/Helper/TransformConstantBufferUpdatingHelper.cpp
+++ src/GraphicsLib/Graphics/GPUResource/TransformConstantBuffer/Helper/TransformConstantBufferUpdatingHelper.cpp
@@ -8,9 +8,17 @@ namespace cg
 {
 	void TransformConstantBufferHelper::storeWVP(DirectX::XMFLOAT4X4* wvp, const Transform& transform, const Camera& camera)
 	{
+		DirectX::XMFLOAT4X4 w;
+		storeW(&w, transform);
 		DirectX::XMFLOAT4X4 vp;
 		storeVP(&vp, camera);
-		DirectX::XMStoreFloat4x4(wvp, DirectX::XMMatrixTranspose(DirectX::XMMatrixMultiply(DirectX::XMLoadFloat4x4(&transform.createWorldMatrix()), DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(&vp)))));
+		DirectX::XMStoreFloat4x4(wvp, DirectX::XMMatrixTranspose(DirectX::XMMatrixMultiply(DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(&w)), DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(&vp)))));
+	}
+
+	void TransformConstantBufferHelper::storeInvWVP(DirectX::XMFLOAT4X4* invWVP, const Transform& transform, const Camera& camera)
+	{
+		storeWVP(invWVP, transform, camera);
+		DirectX::XMStoreFloat4x4(invWVP, DirectX::XMMatrixTranspose(DirectX::XMMatrixInverse(nullptr, DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(invWVP)))));
 	}
 
 	void TransformConstantBufferHelper::storeW(DirectX::XMFLOAT4X4* w, const Transform& transform)
@@ -18,13 +26,51 @@ namespace cg
 		DirectX::XMStoreFloat4x4(w, DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(&transform.createWorldMatrix())));
 	}
 
+	void TransformConstantBufferHelper::storeInvW(DirectX::XMFLOAT4X4* invW, const Transform& transform)
+	{
+		storeW(invW, transform);
+		DirectX::XMStoreFloat4x4(invW, DirectX::XMMatrixTranspose(DirectX::XMMatrixInverse(nullptr, DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(invW)))));
+	}
+
+	void TransformConstantBufferHelper::storeV(DirectX::XMFLOAT4X4* v, const Camera& camera)
+	{
+		storeInvV(v, camera);
+		DirectX::XMStoreFloat4x4(v, DirectX::XMMatrixTranspose(DirectX::XMMatrixInverse(nullptr, DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(v)))));
+	}
+
+	void TransformConstantBufferHelper::storeInvV(DirectX::XMFLOAT4X4* invV, const Camera& camera)
+	{
+		DirectX::XMStoreFloat4x4(invV, DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(&camera.getTransformRef().createWorldMatrix())));
+	}
+
+	void TransformConstantBufferHelper::storeP(DirectX::XMFLOAT4X4* p, const Camera& camera)
+	{
+		DirectX::XMStoreFloat4x4(p, DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(&camera.projection.createMatrix())));
+	}
+
+	void TransformConstantBufferHelper::storeInvP(DirectX::XMFLOAT4X4* invP, const Camera& camera)
+	{
+		storeP(invP, camera);
+		DirectX::XMStoreFloat4x4(invP, DirectX::XMMatrixTranspose(DirectX::XMMatrixInverse(nullptr, DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(invP)))));
+	}
+
 	void TransformConstantBufferHelper::storeVP(DirectX::XMFLOAT4X4* vp, const Camera& camera)
 	{
-		DirectX::XMStoreFloat4x4(vp, DirectX::XMMatrixTranspose(DirectX::XMMatrixMultiply(DirectX::XMMatrixInverse(nullptr, DirectX::XMLoadFloat4x4(&camera.getTransformRef().createWorldMatrix())), DirectX::XMLoadFloat4x4(&camera.projection.createMatrix()))));
+		DirectX::XMFLOAT4X4 v;
+		storeV(&v, camera);
+		DirectX::XMFLOAT4X4 p;
+		storeP(&p, camera);
+		DirectX::XMStoreFloat4x4(vp, DirectX::XMMatrixTranspose(DirectX::XMMatrixMultiply(DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(&v)), DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(&p)))));
+	}
+
+	void TransformConstantBufferHelper::storeInvVP(DirectX::XMFLOAT4X4* invVP, const Camera& camera)
+	{
+		storeVP(invVP, camera);
+		DirectX::XMStoreFloat4x4(invVP, DirectX::XMMatrixTranspose(DirectX::XMMatrixInverse(nullptr, DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(invVP)))));
 	}
 
 	void TransformConstantBufferHelper::storeN(DirectX::XMFLOAT4X4* n, const Transform& transform)
 	{
-		DirectX::XMStoreFloat4x4(n, DirectX::XMMatrixInverse(nullptr, DirectX::XMLoadFloat4x4(&transform.createWorldMatrix())));
+		DirectX::XMStoreFloat4x4(n, DirectX::XMMatrixTranspose(DirectX::XMMatrixInverse(nullptr, DirectX::XMMatrixTranspose(DirectX::XMLoadFloat4x4(&transform.createWorldMatrix())))));
 	}
 }
