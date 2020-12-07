e18d3c1412edc67c52c4065751f046e83f132f7c
Ryota Nakashima <b1615946@planet.kanazawa-it.ac.jp>
2020-12-07 20:05:28 +0900

    change: void resolve()を保持したTexture2Dの親クラスをITexture2Dから新しく作成したクラスIResolvableTexture2Dに変更

---
+++ inc/GraphicsLib/Graphics/GPUResource/ShaderResource/Texture/Base/IResolvable.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+
+
+
+namespace cg
+{
+	class IResolvable
+	{
+	public:
+		virtual void resolve() = 0;
+	};
+}
--- inc/GraphicsLib/Graphics/GPUResource/ShaderResource/Texture/Texture2D/IDefaultTexture2D.hpp
+++ inc/GraphicsLib/Graphics/GPUResource/ShaderResource/Texture/Texture2D/IDefaultTexture2D.hpp
@@ -3,7 +3,7 @@
 #include <GraphicsLib/Graphics/GPUResource/ShaderResource/Components/GPUAccessFlags.hpp>
 #include <GraphicsLib/Graphics/GPUResource/ShaderResource/Base/IShaderResource.hpp>
 #include <ThirdParty/CPPLib/Math/Vector/Vector4D.hpp>
-#include <GraphicsLib/Graphics/GPUResource/ShaderResource/Texture/Texture2D/Base/ITexture2D.hpp>
+#include <GraphicsLib/Graphics/GPUResource/ShaderResource/Texture/Texture2D/Base/IResolvableTexture2D.hpp>
 #include <GraphicsLib/Graphics/GPUResource/ShaderResource/Texture/Texture2D/Components/ImageXY.hpp>
 
 #include <memory>
@@ -15,11 +15,9 @@
 namespace cg
 {
 	class IDefaultTexture2D
-		: public virtual ITexture2D
+		: public virtual IResolvableTexture2D
 	{
 	public:
-		virtual void resolve() = 0;
-
 		virtual void copy(const std::shared_ptr<ITexture2D> dest) = 0;
 		virtual void write(const ImageXY& image) = 0;
 	};
