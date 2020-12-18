1871c5f10112f2698d8c58cf0632fda6d1145258
Ryota Nakashima <b1615946@planet.kanazawa-it.ac.jp>
2020-12-18 13:21:32 +0900

    change: DrawableObjectGroup -> Scene::Group

--- inc/GraphicsLib/Graphics/Scene/Scene.hpp
+++ inc/GraphicsLib/Graphics/Scene/Scene.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <GraphicsLib/Graphics/Scene/Components/DrawableObjectGroup.hpp>
+#include <GraphicsLib/Graphics/DrawableObject/DrawableObject.hpp>
 #include <GraphicsLib/Graphics/Scene/Components/Light/DirectionalLight.hpp>
 #include <GraphicsLib/Graphics/Scene/Components/Light/PointLight.hpp>
 #include <GraphicsLib/Graphics/Scene/Components/Light/SpotLight.hpp>
@@ -13,9 +13,55 @@ namespace cg
 	class Scene
 	{
 	public:
-		using ManagedObjectGroupNameList = std::vector<std::string>;
+		class Group
+			: public cpp::Observer
+		{
+		public:
+			using OperationPerObject = std::function<void(std::shared_ptr<DrawableObject>)>;
+			using OperationPerObjectPart = std::function<void(const DrawableObject::Part&)>;
+			using DrawableObjectDict = std::unordered_map<ID::Type, std::shared_ptr<DrawableObject>>;
+		private:
+			using RenderingGroup = std::shared_ptr<DrawableObjectDict>;
+			using RenderingGroupDictSearchWithRenderPipelineName = std::unordered_map<std::string, RenderingGroup>;
+			using RenderingGroupDictSearchWithObjectID = std::unordered_map<ID::Type, RenderingGroup>;
+		private:
+			std::string m_name;
 
-		using DrawableObjectGroupDict = std::unordered_map<std::string, std::shared_ptr<DrawableObjectGroup>>;
+			RenderingGroupDictSearchWithRenderPipelineName m_renderingGroupDictSearchWithRenderPipelineName;
+			RenderingGroupDictSearchWithObjectID m_renderingGroupDictSearchWithObjectID;
+			DrawableObjectDict m_objectDict;
+		private:
+			[[nodiscard]] bool m_renderingGroupExists(const std::string& groupName) const noexcept;
+
+			void update(cpp::Subject* pSubject) override;
+		public:
+			Group(const std::string& name);
+			virtual ~Group() = default;
+
+
+
+			[[nodiscard]] std::string name() const noexcept;
+
+
+
+			[[nodiscard]] bool objectExists(const cg::ID& objectID) const noexcept;
+
+
+
+			void add(std::shared_ptr<DrawableObject> objectRef);
+			void remove(const cg::ID& objectID);
+			void removeAll();
+
+
+			DrawableObjectDict makeObjectDict() const;
+
+
+			void draw(const std::string& renderingGroupName, const OperationPerObject& operationPerObject, const OperationPerObjectPart& operationPerObjectPart) const;
+		};
+	public:
+		using GroupNameList = std::vector<std::string>;
+
+		using GroupDict = std::unordered_map<std::string, std::shared_ptr<Scene::Group>>;
 
 		using LightDict = std::unordered_map<std::string, std::shared_ptr<Light>>;
 		using LightGroup = std::unordered_map<std::string, std::shared_ptr<Light>>;
@@ -23,7 +69,7 @@ namespace cg
 		using LightGroupDictSearchWithType = std::unordered_map<Light::Type, std::shared_ptr<LightGroup>>;
 		using LightGroupDictSearchWithName = std::unordered_map<std::string, std::shared_ptr<LightGroup>>;
 	private:
-		DrawableObjectGroupDict m_objectGroupDict;
+		GroupDict m_groupDict;
 
 		LightDict m_lightDict;
 		LightGroupDictSearchWithType m_lightGroupDictSearchWithType;
@@ -40,13 +86,13 @@ namespace cg
 
 
 
-		[[nodiscard]] bool objectGroupExists(const std::string& name) const noexcept;
-		void addObjectGroup(const std::shared_ptr<DrawableObjectGroup>& group);
-		void removeObjectGroup(const std::string& groupName);
-		void removeAllObjectGroups();
+		[[nodiscard]] bool groupExists(const std::string& name) const noexcept;
+		void addGroup(const std::shared_ptr<Scene::Group>& group);
+		void removeGroup(const std::string& groupName);
+		void removeAllGroups();
 
-		ManagedObjectGroupNameList createManagedObjectGroupNameList() const;
-		std::shared_ptr<DrawableObjectGroup> getObjectGroup(const std::string& groupName) const;
+		GroupNameList makeGroupNameList() const;
+		std::shared_ptr<Scene::Group> getGroup(const std::string& groupName) const;
 
 
 
@@ -55,7 +101,7 @@ namespace cg
 		void removeLight(const std::string& name);
 		void removeAllLights();
 		
-		[[nodiscard]] LightDict createLightDict() const;
+		[[nodiscard]] LightDict makeLightDict() const;
 		[[nodiscard]] LightGroup getLights(const Light::Type& type) const;
 		template <typename Light_>
 		[[nodiscard]] std::shared_ptr<Light_> getLight(const std::string& name) const
@@ -65,6 +111,6 @@ namespace cg
 
 
 
-		void draw(const std::string renderingGroupName, const DrawableObjectGroup::OperationPerObject& operationPerObject, const DrawableObjectGroup::OperationPerObjectPart& operationPerObjectPart) const;
+		void draw(const std::string renderingGroupName, const Scene::Group::OperationPerObject& operationPerObject, const Scene::Group::OperationPerObjectPart& operationPerObjectPart) const;
 	};
 }
